db1eade21b038c0f0e40db3349d1ff411968be10
Brenda <60548352+BrendaL94@users.noreply.github.com>
2021-12-07 15:22:02 +0200

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,22 @@
+
+file = open("input.txt", "r")
+depth = file.read().splitlines()
+depth = [int(i) for i in depth]
+file.close()
+
+# part one
+increased_counter = 0
+for i in range(1, len(depth)):
+    if depth[i] > depth[i-1]:
+        increased_counter+=1
+    
+print(increased_counter)
+
+# part two
+increased_counter = 0
+for i in range(0, len(depth)):
+    if sum(depth[i:i+3]) < sum(depth[i+1:i+1+3]):
+        increased_counter+=1
+        
+print(increased_counter)
+
